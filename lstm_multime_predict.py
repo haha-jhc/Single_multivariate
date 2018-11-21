ae10b7a4bb677dd0b758056d6c633385c224a1d4
haha-jhc <1253167553@qq.com>
2018-11-21 12:08:21 +0800

    对多变量时间序列进行预测，加入时间周期维度

--- lstm_multime_predict.py
+++ lstm_multime_predict.py
@@ -14,29 +14,21 @@ model = load_model('model_1_multime.h5')
 # model = load_model('model_5_multime.h5')
 # model = load_model('model_10_multime.h5')
 
-y_hat =model.predict(X_test)
-y_hat = y_scale.inverse_transform(y_hat)
+yhat =model.predict(X_test)
+yhat = y_scale.inverse_transform(yhat)
 y_test = y_scale.inverse_transform(y_test)
 
-rmse = math.sqrt(mean_squared_error(y_test,y_hat))
+rmse = math.sqrt(mean_squared_error(y_test,yhat))
 print('Test RMSE:%.3f' % rmse)
 
 plt.rcParams['figure.figsize'] = (20,6)
 plt.rcParams['font.sans-serif']=['SimHei']
 
-# label = ["dataset", "testPredict"]
-# y_test_plt=y_test[ : 24 * 60 ]
-# y_hat_plt=yhat[ : 24 * 60]
-# l1 = plt.plot(y_test_plt,color='green')
-# l2 = plt.plot(y_hat_plt[1:],color='orange')
-# plt.title("预测第1天的预测结果")
-# plt.show()
 
-for i in range(0,7):
-    label = ["dataset", "testPredict"]
-    y_test_plt = y_test[24 * 60 * i:24 * 60 * (i + 1)]
-    y_hat_plt = y_hat[24 * 60 * i:24 * 60 * (i + 1)]
-    l1 = plt.plot(y_test_plt,color='green')
-    l2 = plt.plot(y_hat_plt,color='orange')
-    plt.title("预测第%d天的预测结果"%(i+1))
-    plt.show()
+label = ["dataset", "testPredict"]
+y_test_plt=y_test[ : 24 * 60 ]
+y_hat_plt=yhat[ : 24 * 60]
+l1 = plt.plot(y_test_plt,color='green')
+l2 = plt.plot(y_hat_plt[1:],color='orange')
+plt.title("预测第1天的预测结果")
+plt.show()
